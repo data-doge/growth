3daba554824092d700cbf643d17b9ffdb92a9cdf
euglazer <eugenestapler@gmail.com>
2015-06-16 21:00:24 +1200

    render on load, and add material to body

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,37 +1,46 @@
 $(document).ready(function () {
 
+  // setup scene, camera, and renderer
   var scene = new THREE.Scene();
-  var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
+  var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
   var renderer = new THREE.WebGLRenderer();
   renderer.setSize( window.innerWidth, window.innerHeight );
   document.body.appendChild( renderer.domElement );
+  camera.position.z = 150;
 
-  var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-  var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-
-  camera.position.z = 100;
-
+  // setup orbit controls
   var controls = new THREE.OrbitControls(camera, renderer.domElement);
   controls.target = new THREE.Vector3(0, 0, 0);
 
-  // copied form STLLoader github vvvv
-
+  // load stl file for human body
   var body;
-
   var loader = new THREE.STLLoader();
-  console.log(loader);
-  loader.load( './media/body.stl', function ( geometry ) {
-    body = new THREE.Mesh( geometry );
+  loader.load( './media/body.stl', function (geometry) {
+    var bodyMaterial = new THREE.MeshBasicMaterial( {color: 0xffffff} );
+    body = new THREE.Mesh( geometry, bodyMaterial );
+    body.position.y = 20;
     scene.add( body );
-  } );
+  });
 
+  var floorGeometry = new THREE.PlaneGeometry( 120, 120);
+  var floorMaterial = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
+  var floor = new THREE.Mesh(floorGeometry, floorMaterial);
+  floor.rotation.x = -Math.PI/2.1;
+  floor.rotation.z = -Math.PI/4;
+  scene.add(floor);
+
+  // setup start animation
+  var counter = 0;
   function render() {
     requestAnimationFrame( render );
+    counter += 4;
     renderer.render( scene, camera );
     body.rotation.z += 0.1;
     body.rotation.y += 0.1;
+    body.position.y = counter % 100 + 10;
   }
-  render();
+  $(window).load(function () {
+    render();
+  })
 
 });
